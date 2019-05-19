6c9b33febd7f93480e5ecb17b89ff59bd2db1f8e
Krzysztof Bobecki <Krzysztof Bobecki@192.168.0.46>
2019-05-19 19:41:02 +0200

    This is test commit.

---
+++ testProject/src/testProject/Main.java
@@ -0,0 +1,23 @@
+package testProject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("test Class");
+
+        List<String> firstNames = new ArrayList<String>();
+        
+        firstNames.add("Hello");
+        firstNames.add("Beautiful");
+        firstNames.add("World!");
+        
+        System.out.println(firstNames);
+        
+    }
+
+}
